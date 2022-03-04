11e915a713a4a49207eba7540055907b6c60c4e5
Gareth Billett <gareth.billett@eset.com>
2022-03-04 17:01:10 +0000

    replicate main branch

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 let setDefaults = true,
 parameterList;
-enableWindows.checked = true;
+enableWindows.checked = true,
+darkMode = true;
+themeSwitcher.setAttribute("style", "filter: invert(100%)");
 function update() {
     let s = "",
     isOutputValid = 0;
     enableWindows.checked ? baseDirectory = "c:\\mirrorTool\\" : baseDirectory = "/tmp/mirrorTool/";
+    darkMode ? document.querySelector("html").style.filter = "invert(0)" : document.querySelector("html").style.filter = "invert(100%)";
     if (setDefaults){
         enableMirror.checked = true;
         enableRepository.checked = false;
         enableGlobal.checked = false;
         enableOptional.checked = false;
-        //master list of parameters
+        //Master list of parameters
         //KEY: 0=name of parameter, 1=default value, 2=type of element, 3=section name, 4=optional
         parameterList = [
             ["mirrorType", "regular", "select", "mirror", false],
@@ -36,7 +39,7 @@ function update() {
         ];
     }
     for (let i = 0; i < parameterList.length; i++) {
-        //aliases
+        //Aliases
         let pName = parameterList[i][0];
         let pElement = document.getElementById(pName);
         let pDefault = parameterList[i][1];
@@ -49,34 +52,34 @@ function update() {
             pElement.checked = pDefault;
         }
         let o = document.getElementsByClassName("optional");
-        //iterate through optional parameters, hide them if enableoptional is not checked
+        //Iterate through optional parameters, hide them if enableoptional is not checked
         for (let i = 0; i < o.length; i++) {
             enableOptional.checked ? o[i].style.display = "block" : o[i].style.display = "none";
         }
-        //iterate through all the parameters
+        //Iterate through all the parameters
         if (pElement != null) {
-            //check if section is enabled, if so allow the mandatory parameters to be written to the output
+            //Check if section is enabled, if so allow the mandatory parameters to be written to the output
             if ((enableMirror.checked && pSection == "mirror") || (enableRepository.checked && pSection == "repository") || (enableGlobal.checked && pSection == "global")) {
-                //check if either optional parameters are enabled or optional parameters are disabled and current parameter is mandatory
+                //Check if either optional parameters are enabled or optional parameters are disabled and current parameter is mandatory
                 if (enableOptional.checked || !enableOptional.checked && parameterList[i][4] == false) {
                     switch (pType) {
                         case ("text"):
-                            //write parameter and args for text box
+                            //Write parameter and args for text box
                             if (pElement.value != "") s += "--" + pName + " " + pElement.value + " ";
                             break;
                         case ("checkbox"):
-                            //write parameter for checkbox
+                            //Write parameter for checkbox
                             if (pElement.checked) s += "--" + pName + " ";
                             break;
                         case ("select"):
-                            //write parameter for currently selected item in dropdown box and args
+                            //Write parameter for currently selected item in dropdown box and args
                             if (pElement.options[pElement.selectedIndex].text != "none") s += "--" + pName + " " + pElement.options[pElement.selectedIndex].value + " ";
                             break;
                     }
                 }
             }
         }
-        //if field is empty and mandatory then highlight the field red, modify the placeholder text, and declare the output as invalid
+        //If field is empty and mandatory then highlight the field red, modify the placeholder text, and declare the output as invalid
         if (pElement.value == "" && !pOptional) {
             pElement.style.borderColor = "red";
             pElement.placeholder = "This field cannot be blank";
@@ -85,7 +88,7 @@ function update() {
             pElement.style.borderColor = "rgb(63, 63, 63)";
         }
     }
-    //if the number of invalid fields are more than 0 or mandatory sections are disabled the disable the copy and download buttons, otherwise show them
+    //If the number of invalid fields are more than 0 or mandatory sections are disabled the disable the copy and download buttons, otherwise show them
     if (isOutputValid > 0 || (!enableMirror.checked && !enableRepository.checked)) {
         copyButton.disabled = true;
         downloadButton.disabled = true;
@@ -95,17 +98,17 @@ function update() {
         downloadButton.disabled = false;
         commandPreview.disabled = false;
     }
-    //trim whitespace
+    //Trim whitespace
     s = s.trim();
-    //check if there is anything to write and if the output is valid, if so write the platform specific prefix plus the commands to the command preview
+    //Check if there is anything to write and if the output is valid, if so write the platform specific prefix plus the commands to the command preview
     if (s.length != 0 && isOutputValid == 0) {
         enableWindows.checked ? commandPreview.innerHTML = "MirrorTool.exe " + s : commandPreview.innerHTML = "sudo ./MirrorTool " + s
-    } else commandPreview.innerHTML = "";
-    //show or hide sections based on checkbox states
+    } else commandPreview.innerHTML = "Some parameter sections are not enabled, or mandatory fields are empty. Check your configuration.";
+    //Show or hide sections based on checkbox states
     enableMirror.checked ? mirror.style.display = "block" : mirror.style.display = "none";
     enableRepository.checked ? repository.style.display = "block" : repository.style.display = "none";
     enableGlobal.checked ? global.style.display = "block" : global.style.display = "none";
-    //workaround for auto-sizing the command line preview box
+    //Workaround for auto-sizing the command line preview box
     commandPreview.setAttribute("style", "height: 0px");
     commandPreview.setAttribute("style", "height:" + (commandPreview.scrollHeight) + "px;overflow-y:hidden;");
     setDefaults = false;
@@ -118,9 +121,16 @@ var clipboard = new Clipboard(document.getElementById('copyButton'), {
         return commandPreview.innerHTML;
     }
 });
-//event listeners for updating command line preview
+//Main form event listener to update the command preview
+main.addEventListener("input", function() { update(); });
+//Dismiss info event listener
+dismissLink.addEventListener("click", function() { info.style.display = "none" });
+//Event listeners for reset query
+resetLink.addEventListener("click", function() { resetQuestion() });
+enableWindows.addEventListener("click", function () { resetQuestion() });
+enableLinux.addEventListener("click", function () { resetQuestion() });
+//Download event listener
 downloadButton.addEventListener("click", function (event) {
-    if (commandPreview.innerHTML != ""){
         if (enableWindows.checked) {
             download('test.bat', commandPreview.innerHTML);
          } else {
@@ -128,35 +138,30 @@ downloadButton.addEventListener("click", function (event) {
             s = "#!/usr/bin/env bash\n" + (s.split("sudo ").pop());
             download('test.sh', s);
          }
-    } else alert("Command line cannot be empty");
 });
-let input = document.querySelectorAll("input");
-for (i = 0; i < input.length; i++) {
-    input[i].addEventListener("input", function () {
-        update();
-    });
-}
-mirrorType.addEventListener("input", function () { update(); });
-mirrorFileFormat.addEventListener("input", function () { update(); });
-excludedProducts.addEventListener("input", function () { update(); });
-enableWindows.addEventListener("click", function () {
-        setDefaults = question();
-        update(); 
-});
-enableLinux.addEventListener("click", function () {
-        setDefaults = question();
-        update();
-});
-//scroll to bottom when section expands to ensure visibility
-mirror.addEventListener("input", function() { scrollToBottom() });
-repository.addEventListener("input", function() { scrollToBottom() });
-global.addEventListener("input", function() { scrollToBottom() });
+//Scroll to bottom when section expands to ensure visibility
+enableMirror.addEventListener("input", function() { scrollToBottom() });
+enableRepository.addEventListener("input", function() { scrollToBottom() });
 enableOptional.addEventListener("input", function() { scrollToBottom() });
+enableGlobal.addEventListener("input", function() { scrollToBottom() });
+//Theme switcher event listener
+themeSwitcher.addEventListener("click", function() {
+    darkMode ? darkMode = false : darkMode = true;
+    update();
+});
+//Prevent accidental F5 keypress
+document.addEventListener('keydown', (e) => {
+    e = e || window.event;
+    if(e.keyCode == 116){
+        e.preventDefault();
+    }
+});
 function scrollToBottom(){
     window.scrollTo(0,document.body.scrollHeight);
 }
-function question(){
-    return (confirm("Reset all mandatory parameters to their platform specific default values?"));
+function resetQuestion(){
+    setDefaults = confirm("Reset all settings and filters?");
+    update();
 }
 function download(filename, text) {
     var pom = document.createElement('a');
